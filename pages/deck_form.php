2d5b96a5a50e52de44e5acc43e97af5aa0f4be23
Kazunori Kimura <kazunori.abu@gmail.com>
2014-12-28 15:58:30 +0900

    save previous input

--- pages/deck_form.php
+++ pages/deck_form.php
@@ -1,12 +1,12 @@
 <div id="register_deck_div">
 	<div class="header"><h2 class="menu_title"><?php echo __("Register New Deck", 'mtg-deck-util') . "<br/>" ?></h2></div>
 	<form action="<?php echo esc_url( $_SERVER['REQUEST_URI']) ?>" method="post">
-		<div class="elem_name"><?php echo __( 'Player', 'mtg-deck-util' ) ?>:</div>
-		<div class="elem"><input type="text" name="player_name" size="40"></div>
 		<div class="elem_name"><?php echo __( 'Deckname', 'mtg-deck-util' ) ?>:</div>
-		<div class="elem"><input type="text" name="deck_name" size="40"></div>
+		<div class="elem"><input type="text" name="deck_name" size="40" value="<?php echo $deckname ?>"></div>
+		<div class="elem_name"><?php echo __( 'Player', 'mtg-deck-util' ) ?>:</div>
+		<div class="elem"><input type="text" name="player_name" size="40" value="<?php echo $player ?>"></div>
 		<div class="elem_name"><?php echo __( 'Reference Key', 'mtg-deck-util' ) ?>:</div>
-		<div class="elem"><input type="text" name="ref_key" size="40"></div>
+		<div class="elem"><input type="text" name="ref_key" size="40" value="<?php echo $refkey ?>"></div>
 		<div class="elem_name"><?php echo __( 'Format', 'mtg-deck-util' ) ?>:</div>
 		<div class="elem">
 			<select name="format">
@@ -18,7 +18,7 @@
 			</select>
 		</div>
 		<div class="elem_name"><?php echo __( 'Decklist', 'mtg-deck-util' ) ?>:</div>
-		<div class="elem"><textarea name="decklist" align="left" rows="20" cols="40"></textarea></div>
-		<div class="submit"><input type="submit" align="right"" value=<?php echo __('Register Deck', 'mtg-deck-util') ?>></div>
+		<div class="elem"><textarea name="decklist" align="left" rows="20" cols="40"><?php echo $decklist ?></textarea></div>
+		<div class="submit"><input type="submit" name="deck-submitted" align="right"" value=<?php echo __('Register Deck', 'mtg-deck-util') ?>></div>
 	</form>
 </div>
