1db29573073d763fd0f16331fbd94fd7536b4243
Kazunori Kimura <kazunori.abu@gmail.com>
2014-12-28 14:48:20 +0900

    separate php file

--- mtg-deck-util.php
+++ mtg-deck-util.php
@@ -42,6 +42,12 @@ class MtGDeckUtil{
 		if ( __FILE__ != WP_UNINSTALL_PLUGIN )
 			return;
 	}
+
+	/// create deck database to wordpress db
+	/// on activation
+	static function db_install(){
+		global $wpdb;
+	}
 	
 	/// Initialize the plugin
 	function __construct(){
@@ -60,6 +66,7 @@ class MtGDeckUtil{
 	}
 
 	function plugin_menu(){
+		
 		add_menu_page(
 			__('Magic the Gaghering Deck Utility', 'mtg-deck-util'),
 			__('MtG Deck Utility', 'mtg-deck-util'),
@@ -67,6 +74,7 @@ class MtGDeckUtil{
 			'mtgdeckutil',
 			array( $this, 'menu_page' )
 		);
+		
 		$deck_menu =  add_submenu_page(
 			'mtgdeckutil',
 			__('Add New Deck', 'mtg-deck-util'),
@@ -93,34 +101,8 @@ class MtGDeckUtil{
 			'Modern' => __('Modern', 'mtg-deck-util'),
 			'Legacy' => __('Legacy', 'mtg-deck-util')
 		);
-?>
-	<div id="register_deck_div">
-		<div class="header"><h2 class="menu_title"><?php echo __("Register New Deck", 'mtg-deck-util') . "<br/>" ?></h2></div>
-		<form action="<?php echo esc_url( $_SERVER['REQUEST_URI']) ?>" method="post">
-			<div class="elem_name"><?php echo __( 'Player', 'mtg-deck-util' ) ?>:</div>
-			<div class="elem"><input type="text" name="player_name" size="40"></div>
-			<div class="elem_name"><?php echo __( 'Deckname', 'mtg-deck-util' ) ?>:</div>
-			<div class="elem"><input type="text" name="deck_name" size="40"></div>
-			<div class="elem_name"><?php echo __( 'Reference Key', 'mtg-deck-util' ) ?>:</div>
-			<div class="elem"><input type="text" name="ref_key" size="40"></div>
-			<div class="elem_name"><?php echo __( 'Format', 'mtg-deck-util' ) ?>:</div>
-			<div class="elem">
-				<select name="format">
-					<?php
-					foreach ( $formats as $t => $name ) {
-						echo "<option value=\"$t\">$name</option>";
-					}
-					?>
-				</select>
-			</div>
-			<div class="elem_name"><?php echo __( 'Decklist', 'mtg-deck-util' ) ?>:</div>
-			<div class="elem"><textarea name="decklist" align="left" rows="20" cols="40"></textarea></div>
-			<div class="submit"><input type="submit" align="right"" value=<?php echo __('Register Deck', 'mtg-deck-util') ?>></div>
-		</form>
-	</div>
-<?php
+		include(__DIR__ . "/pages/deck_form.php");
 	}
-	
 }
 
 function MtGDeckUtil(){
