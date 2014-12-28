4132575604b30ee1b98d3c6ae1ef7997bc59efac
Kazunori Kimura <kazunori.abu@gmail.com>
2014-12-28 11:27:36 +0900

    edit meta data

--- mtg-deck-util.php
+++ mtg-deck-util.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Mtg-deck-util
 Version: 0.1-alpha
-Description: PLUGIN DESCRIPTION HERE
-Author: YOUR NAME HERE
-Author URI: YOUR SITE HERE
+Description: Magic the Gathering Deck Utilities for Wordpress
+Author: Kazunori Kimura
+Author URI: http://twitter.com/trairia
 Plugin URI: PLUGIN SITE HERE
 Text Domain: mtg-deck-util
 Domain Path: /languages
