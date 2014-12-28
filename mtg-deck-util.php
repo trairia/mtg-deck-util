e386d276a5b7e6143c03f89a07200d0f7f37b1fd
Kazunori Kimura <kazunori.abu@gmail.com>
2014-12-28 11:35:47 +0900

    add activate, deactivate, uninstall callback

--- mtg-deck-util.php
+++ mtg-deck-util.php
@@ -10,3 +10,45 @@ Text Domain: mtg-deck-util
 Domain Path: /languages
 */
 
+/// register hooks
+register_activation_hook(   __FILE__, array( 'MtGDeckUtil', 'on_activation' ) );
+register_deactivation_hook( __FILE__, array( 'MtGDeckUtil', 'on_deactivation' ) );
+register_uninstall_hook(    __FILE__, array( 'MtGDeckUtil', 'on_uninstall' ) );
+/// Plugin Implement
+class MtGDeckUtil{
+
+	/// Initialize the plugin
+	function __construct(){
+	}
+
+	/// callback to register_activation_hook
+	public static function on_activate(){
+		if ( ! current_user_can( 'activate_plugins' ) )
+			return;
+		$plugin = self::get_plugin();
+		check_admin_referer( "activate-plugin_${plugin}" );
+	}
+
+	/// callback to register_deactivation_hook
+	public static function on_deactivate(){
+		if ( ! current_user_can( 'activate_plugins' ) )
+			return;
+		$plugin = self::get_plugin();
+		check_admin_referer( "deactivate-plugin_${plugin}" );
+	}
+
+	/// callback to register_uninstall_hook
+	public static function on_uninstall(){
+		if ( ! current_user_can( 'activate_plugins' ) )
+			return;
+		check_admin_referer( 'bulk-plugins' );
+
+		if ( __FILE__ != WP_UNINSTALL_PLUGIN )
+			return;
+	}
+
+	private static function get_plugin(){
+		return isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
+	}
+}
+
