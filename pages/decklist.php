4df760048b0fe05dc01e5efe9f0d5f66a95f1a01
Kazunori Kimura <kazunori.abu@gmail.com>
2015-01-11 23:04:19 +0900

    use cardname formatter

--- pages/decklist.php
+++ pages/decklist.php
@@ -10,23 +10,23 @@ function count_num( $data ){
 	<div class="deck_list_header"><?php echo $deckname ?>/<?php echo $player ?></div>
 	<div class="pile_land">
 		<?php foreach ( $lands as $l) {?>
-			<?php echo $l['num'] ?> <?php echo $l['name'] ?> <br />
+			<?php echo $l['num'] ?> <?php echo $formatter->format( $l ) ?> <br />
 		<?php } ?>
 	</div>
 	<div class="pile_creature">
 		<?php foreach ( $creatures as $c) {?>
-			<?php echo $c['num'] ?> <?php echo $c['name'] ?> <br />
+			<?php echo $c['num'] ?> <?php echo $formatter->format( $c ) ?> <br />
 		<?php } ?>
 	</div>
 	<div class="pile_spell">
 		<?php foreach ( array_merge( $instant, $sorcery, $artifact, $enchant, $planeswalker ) as $s) {?>
-			<?php echo $s['num'] ?> <?php echo $s['name'] ?> <br />
+			<?php echo $s['num'] ?> <?php echo $formatter->format( $s ) ?> <br />
 		<?php } ?>
 	</div>
 	<div class="sideboard">
 		<?php echo __('Sideboard', 'mtg-deck-util') ?> <br />
 		<?php foreach ( $sideboard as $s) {?>
-			<?php echo $s['num'] ?> <?php echo $s['name'] ?> <br />
+			<?php echo $s['num'] ?> <?php echo $formatter->format( $s ) ?> <br />
 		<?php } ?>
 	</div>
 </div>
